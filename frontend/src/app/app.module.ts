6c0398076abd0f6ce5e81ace2a8b1b17da413dfb
Rodrigo <rmarnon@yahoo.com.br>
2021-01-09 11:43:25 -0300

    Sidenav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { RedDirective } from './directives/red.directive';
-import { BlueDirective } from './directives/blue.directive'
+import { BlueDirective } from './directives/blue.directive';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,16 @@ import { BlueDirective } from './directives/blue.directive'
     HeaderComponent,
     FooterComponent,
     RedDirective,
-    BlueDirective
+    BlueDirective,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/directives/blue.directive.ts
+++ frontend/src/app/directives/blue.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class BlueDirective {
 
   constructor(el: ElementRef) {
-    el.nativeElement.style.color = '#0064ff';
+    el.nativeElement.style.color = '#3232ff';
    }
 
 }
